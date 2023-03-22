a6549582ed4143e0462d62e9680987df8e2e44be
minnehkamau <minnieches08@gmail.com>
2023-03-22 14:50:06 +0300

    migration

--- db/migrate/20230322114310_create_signups.rb
+++ db/migrate/20230322114310_create_signups.rb
@@ -1,6 +1,8 @@
 class CreateSignups < ActiveRecord::Migration[6.1]
   def change
     create_table :signups do |t|
+      t.references :camper, null: false, foreign_key: true
+      t.references :activity, null: false, foreign_key: true
       t.integer :time
 
       t.timestamps
